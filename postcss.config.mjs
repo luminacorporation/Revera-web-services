7f8fedc3b99e157d589e7985e13d88e6bfeb50e5
Kaushal Mishra <mishplayz52@gmail.com>
2026-07-16 12:31:35 +1000

    Remove remaining merge conflict

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-}
-
-export default config
->>>>>>> 810ebb87c5facaf3619eac103b43e2cd99847ea1
